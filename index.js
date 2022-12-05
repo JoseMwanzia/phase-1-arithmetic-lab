97d362c7f0b7585c78cead6e7a46f3af638106f1
joseph mwanzia <makamwanzia@gmail.com>
2022-12-05 18:32:13 +0300

    Complete Arithimetic labs

--- index.js
+++ index.js
@@ -1,2 +1,33 @@
 
 // Write your code here
+
+//Create a variable called multiply set to an equation that will
+//  multiply the variables num1 and num2; the result of the
+//  multiplication should be 62.
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+console.log(multiply)
+
+// Create a variable called random that will generate a random
+//  integer greater than 0.
+let random = Math.ceil(Math.random() * 10);
+if (random > 0) {
+  console.log(random);
+}
+
+// Create a variable called mod set to an equation that will 
+// calculate the remainder of dividing variable num3 by num4;
+//  the remainder should be 4.
+let num3 = 29;
+let num4 = 5;
+let mod = num3 % num4;
+
+console.log(mod);
+
+// Create a variable called max that finds the highest 
+// number in a set; the value returned should be 20.
+let max = Math.max(2, 3, 15, 20, 19);
+console.log(max);
+    
+
